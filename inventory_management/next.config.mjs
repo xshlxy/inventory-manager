c61d879df927ba6c2b439d91c753274ab2532e46
Ashley <ashley.oliveiraa@outlook.com>
2024-08-04 15:00:57 -0400

    fixes keys

--- inventory_management/next.config.mjs
+++ inventory_management/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        GOOGLE_CLOUD_API_KEY: process.env.GOOGLE_CLOUD_API_KEY,
-      },
+    
 };
 
 export default nextConfig;
